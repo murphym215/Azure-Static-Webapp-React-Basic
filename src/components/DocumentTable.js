d21ed039dc3a05037205544c3e041669c2b455f8
murphym215 <murphym215@gmail.com>
2026-06-20 18:40:24 -0400

    Create DocumentTable.js

---
+++ src/components/DocumentTable.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function DocumentTable({ documents }) {
+  return (
+    <table className="doc-table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Category</th>
+          <th>Description</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {documents.map((doc) => (
+          <tr key={doc.id}>
+            <td>{doc.title}</td>
+            <td>{doc.category}</td>
+            <td>{doc.description}</td>
+            <td>
+              {doc.url}>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default DocumentTable;
